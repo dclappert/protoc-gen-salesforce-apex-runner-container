d3f94952f8c80c6ce1ea1bfb62b97a44788a5b20
David Lappert <dclappert@gmail.com>
2024-01-17 20:28:03 -0500

    wip

--- scripts/docker-build-and-push.sh
+++ scripts/docker-build-and-push.sh
@@ -4,7 +4,7 @@ VERSION=$1
 IMAGE_NAME="ghcr.io/dclappert/protoc-gen-salesforce-apex-runner-container"
 
 # Build the Docker image
-docker build . --file ../Dockerfile --tag $IMAGE_NAME:$VERSION
+docker build . --file Dockerfile --tag $IMAGE_NAME:$VERSION
 
 # Login to GitHub Content Registry
 echo $GITHUB_TOKEN | docker login ghcr.io -u $ --password-stdin
